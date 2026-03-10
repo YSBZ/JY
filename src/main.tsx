1f70c30ea6de3c2613c7a770db10865cf4886334
ys <ys@ysdeMac-mini-2.local>
2026-03-10 18:31:50 +0800

    fix: support github pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import { App } from './App'
 import { LocaleProvider } from './state/locale'
 import './app.css'
 
+const Router = window.location.hostname.endsWith('github.io') ? HashRouter : BrowserRouter
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Router basename={import.meta.env.BASE_URL}>
       <LocaleProvider>
         <App />
       </LocaleProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 )
