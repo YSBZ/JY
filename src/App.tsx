7e3ced18b6acab578462b8057e5d77ca79cd5ac0
ys <ys@ysdeMac-mini-2.local>
2026-03-11 11:09:19 +0800

    feat: sync route titles by locale

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
+import { articles, products, uiCopy } from './content/site'
+import { useLocale } from './state/locale'
 import { Shell } from './ui/Shell'
 import { ArticlePage } from './views/ArticlePage'
 import { ArticlesPage } from './views/ArticlesPage'
@@ -10,9 +13,47 @@ import { ProductPage } from './views/ProductPage'
 import { ProductsPage } from './views/ProductsPage'
 import { StoryPage } from './views/StoryPage'
 
+function RouteMetadata() {
+  const location = useLocation()
+  const { locale } = useLocale()
+  const ui = uiCopy[locale]
+
+  useEffect(() => {
+    const path = location.pathname
+    let title = 'Jiuyu Machinery'
+
+    if (path === '/') {
+      title = 'Jiuyu Machinery'
+    } else if (path === '/products') {
+      title = `${ui.productCatalogTitle} | Jiuyu Machinery`
+    } else if (path.startsWith('/products/')) {
+      const slug = path.replace('/products/', '')
+      const product = products.find((item) => item.slug === slug)
+      title = product ? `${product.title[locale]} | Jiuyu Machinery` : `${ui.productMissing} | Jiuyu Machinery`
+    } else if (path === '/story') {
+      title = `${ui.companyEyebrow} | Jiuyu Machinery`
+    } else if (path === '/articles') {
+      title = `${ui.articlesTitle} | Jiuyu Machinery`
+    } else if (path.startsWith('/articles/')) {
+      const slug = path.replace('/articles/', '')
+      const article = articles.find((item) => item.slug === slug)
+      title = article ? `${article.title[locale]} | Jiuyu Machinery` : `${ui.articleMissing} | Jiuyu Machinery`
+    } else if (path === '/contact') {
+      title = `${ui.inquiryEyebrow} | Jiuyu Machinery`
+    } else {
+      title = `${ui.notFoundTitle} | Jiuyu Machinery`
+    }
+
+    document.title = title
+  }, [locale, location.pathname, ui])
+
+  return null
+}
+
 export function App() {
   return (
     <Shell>
+      <RouteMetadata />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
